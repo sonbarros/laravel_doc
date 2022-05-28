770f89f5aaace7828741a4e79490994980f732ea
Anderson de S Barros <anderson21br@gmail.com>
2022-05-28 15:51:31 -0300

    incluido aula 10

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ColecoesController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/colecoes/aula01', [ColecoesController::class, 'aula01']);
 Route::get('/colecoes/aula02', [ColecoesController::class, 'aula02']);
 Route::get('/colecoes/aula03', [ColecoesController::class, 'aula03']);
 Route::get('/colecoes/aula04', [ColecoesController::class, 'aula04']);
 Route::get('/colecoes/aula05', [ColecoesController::class, 'aula05']);
 Route::get('/colecoes/aula06', [ColecoesController::class, 'aula06']);
+Route::get('/colecoes/aula07', [ColecoesController::class, 'aula07']);
+Route::get('/colecoes/aula08', [ColecoesController::class, 'aula08']);
+Route::get('/colecoes/aula09', [ColecoesController::class, 'aula09']);
+Route::get('/colecoes/aula10', [ColecoesController::class, 'aula10']);
 
 
